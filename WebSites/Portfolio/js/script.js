425afa994438d2f59c78a378d6e9f2e0f33071a3
Peet <kaminski.piotr.mail@gmail.com>
2022-11-03 17:46:44 +0100

    Added hierarchy and portfolio website

---
+++ WebSites/Portfolio/js/script.js
@@ -0,0 +1,18 @@
+﻿window.addEventListener('scroll', reveal);
+
+function reveal() {
+    let reveals = document.querySelectorAll('.content');
+
+    for (let i = 0; i < reveals.length; i++) {
+        if (reveals[i].getBoundingClientRect().top < window.innerHeight - 150 &&
+            reveals[i].getBoundingClientRect().bottom > 0) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+window.onload = function () {
+    let contents = document.querySelectorAll('.content');
+    contents[0].classList.add('active');
+}
