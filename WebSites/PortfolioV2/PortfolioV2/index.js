9311469a05aaeb96143e7339ab9979a3fcee0994
Peet <kaminski.piotr.mail@gmail.com>
2022-12-07 20:50:00 +0100

    Added portfolio

---
+++ WebSites/PortfolioV2/PortfolioV2/index.js
@@ -0,0 +1,13 @@
+﻿const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('shown');
+        } else {
+            //entry.target.classList.remove('shown');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
